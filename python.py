85e53c017b38ee1b1ef69364ac073624e3703d93
andrij2003 <118419168+andrij2003@users.noreply.github.com>
2022-11-20 16:19:03 +0200

    Andrij

---
+++ python.py
@@ -0,0 +1,17 @@
+import requests
+
+response = requests.get('https://httpbin.org/')
+for line in response.iter_lines():
+    print(line)
+
+    INSTALLED_APPS  =  ( 
+    ... , 
+    "django_stripe" , 
+)
+from django.urls import include, path
+
+urlpatterns = [
+    # Assuming we're at the root, this will make the webhook
+    # available at /stripe/webhook/
+    path("stripe/", include("django_stripe.urls", namespace="stripe"))
+]
